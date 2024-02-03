a04aa465892c5ba91f908d3e38b4090b74ded8a1
Jairo Freire <jairofreireortiz10@gmail.com>
2024-02-03 16:08:15 -0500

    Class modified to return to the main page

--- lib/classes/test_eyes.dart
+++ lib/classes/test_eyes.dart
@@ -5,9 +5,16 @@ class TestEyesightPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Prueba de visión")),
-      body: const Center(child: Text("Pagina para test de la vista")),
+    // ignore: deprecated_member_use
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.of(context).popUntil((route) => route.isFirst);
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(title: const Text("Prueba de visión")),
+        body: const Center(child: Text("Pagina para test de la vista")),
+      ),
     );
   }
 }
