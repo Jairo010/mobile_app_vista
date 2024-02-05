68f7fb11ee95e8591906a00c4d50782d8f10a6da
Jairo Freire <jairofreireortiz10@gmail.com>
2024-02-04 21:53:15 -0500

    logo has been added to the homePage

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,25 @@ class _MyHomePageState extends State<_MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Peek Acuity'),
+       appBar: AppBar(
+        title: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                'assets/images/logo.jpeg',
+                height: 50, // ajusta la altura de la imagen según sea necesario
+                width: 50, // ajusta el ancho de la imagen según sea necesario
+              ),
+            ),
+            const Text(
+              'Optihelath',
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+          ],
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
