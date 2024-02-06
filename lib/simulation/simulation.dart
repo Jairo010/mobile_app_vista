78f125cec98ce782e5309f356ae7259e03fdb640
Jairo Freire <jairofreireortiz10@gmail.com>
2024-02-05 21:09:37 -0500

    Simulation page has been added

---
+++ lib/simulation/simulation.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Simulations extends StatefulWidget {
+  const Simulations({Key? key}) : super(key: key);
+  @override
+  SimulationPageState createState() => SimulationPageState();
+}
+
+class SimulationPageState extends State<Simulations> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
