36d1d5cdb643e886a13b14b4be1957df7a19c559
Jairo Freire <jairofreireortiz10@gmail.com>
2024-02-05 21:09:58 -0500

    Tutorials page has been added

---
+++ lib/tutorials/tutorials.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Tutorials extends StatefulWidget {
+  const Tutorials({Key? key}) : super(key: key);
+  @override
+  TutorialsPageState createState() => TutorialsPageState();
+}
+
+class TutorialsPageState extends State<Tutorials> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
