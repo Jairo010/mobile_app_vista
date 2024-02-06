cb64617e11e1f22b2de4435286df3de8ac021caf
Jairo Freire <jairofreireortiz10@gmail.com>
2024-02-06 01:24:32 -0500

    Begin of the third part of the tutorials was added

---
+++ lib/tutorials/instruction_post_pacient.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'final_tutorials.dart';
+
+class InstructionPostPacientPage extends StatefulWidget {
+  const InstructionPostPacientPage({Key? key}) : super(key: key);
+  @override
+  InstructionPostPacientPageState createState() => InstructionPostPacientPageState();
+}
+
+class InstructionPostPacientPageState extends State<InstructionPostPacientPage> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
+
+  List<String> images = [
+    "finalT2.jpg",
+  ];
+
+  List<String> instructions = [
+    "Siguiente paso",
+  ];
+
+  List<String> instructionsDown = [
+    "Aprender como obtener lo mejor de la prueba",
+  ];
+  int currentInstructionIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Instrucciones"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/${images[currentInstructionIndex]}',
+              height: 320,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            Text(
+              instructions[currentInstructionIndex],
+              style: const TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              instructionsDown[currentInstructionIndex],
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(width: 16),
+                ElevatedButton(
+                  onPressed: currentInstructionIndex < instructions.length - 1
+                      ? () {
+                          setState(() {
+                            currentInstructionIndex++;
+                          });
+                        }
+                      : () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const FinalTutorialsPage(),
+                            ),
+                          );
+                        },
+                  child: const Text(
+                         "Siguiente",
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
