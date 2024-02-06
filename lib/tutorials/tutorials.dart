2d2f6a893ec2cbb39753ed63a180079bf182e79a
Jairo Freire <jairofreireortiz10@gmail.com>
2024-02-06 01:16:43 -0500

    First tuto instructions were added

--- lib/tutorials/tutorials.dart
+++ lib/tutorials/tutorials.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'instructions_tutorials.dart';
 
 class Tutorials extends StatefulWidget {
   const Tutorials({Key? key}) : super(key: key);
@@ -7,16 +8,150 @@ class Tutorials extends StatefulWidget {
 }
 
 class TutorialsPageState extends State<Tutorials> {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          
-        ],
+      appBar: AppBar(
+        title: const Text("Instrucciones"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 30),
+            const Center(
+              child: Text(
+                'Ver como ',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 70),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 20, 
+                  height: 20, 
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color.fromARGB(255, 245, 178, 71), 
+                  ),
+                  child: const Center(
+                    child: Text(
+                      '1',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8), 
+                const Text(
+                  'Realizar pruebas precisas',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Roboto',
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16), 
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 20,
+                  height: 20,
+                  decoration: const BoxDecoration(
+                    shape:  BoxShape.circle,
+                    color: Color.fromARGB(255, 245, 178, 71),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      '2',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                const Text(
+                  'Explicar al paciente qué hacer',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Roboto',
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 20,
+                  height: 20,
+                  decoration: const BoxDecoration(
+                    shape:  BoxShape.circle,
+                    color: Color.fromARGB(255, 245, 178, 71),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      '3',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                const Text(
+                  'Obtener lo mejor de la prueba',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Lato',
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 100),
+            const Center(
+              child: Text(
+                'Despues de realizar 5 pruebas, te sentiras mas rapido que en pruebas tradicionales',
+                style:TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 100),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(width: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const InstructionTutorialsPage(),
+                      ),
+                    );
+                  },
+                  child: const Text('Empezar'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
